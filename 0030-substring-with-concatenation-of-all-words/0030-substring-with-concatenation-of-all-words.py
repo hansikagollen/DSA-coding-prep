7462012f5bb653468e558513768244c26e753aa0
hansikagollen <146338862+hansikagollen@users.noreply.github.com>
2025-06-19 10:26:10 +0530

    Time: 7747 ms (21.96%), Space: 18.6 MB (84.45%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        word_l=len(words[0])
+        word_c=len(words)
+        word_length=word_l*word_c
+        if len(s)<word_length:
+            return[]
+
+        from collections import Counter
+        word_counter=Counter(words)
+        start=0
+        res=[]
+
+        for i in range(len(s)-word_length+1):
+            window=s[i:i+word_length]
+            word_length_counter=Counter()
+
+            for j in range(0,word_length,word_l):
+                word=window[j:j+word_l]
+                word_length_counter[word]+=1
+
+            if word_counter==word_length_counter:
+                res.append(i)
+
+        return res    
+
+        
+        
